b601703adcff2786abb5e9161c41018099912075
Hágata Mendes <hagatamendes2017@gmail.com>
2025-04-05 12:10:38 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import models.Carro;
+import models.Fabricante;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Carro carro = new Carro();
+
+        carro.Id = 777L;
+        carro.Modelo = "Onix";
+        carro.Ano = 2024;
+        carro.Cor = "Preto";
+
+        carro.ExibirInformacao();
+
+        System.out.println("Conceito de Abstração!!!");
+        carro.MoverVeiculo();
+
+        System.out.println("Conceito de Polimorfismo!!!");
+        Fabricante fab = new Fabricante();
+
+        fab.
+
+    }
+}
